776048cbc4de947d16b2b47af1ebdca15a415cab
Rafael Ucha Campos <144175928+rafaeluchacampos@users.noreply.github.com>
2023-12-11 10:11:35 -0300

    Update classificador.js
    Add if statements

--- classificador.js
+++ classificador.js
@@ -1,5 +1,24 @@
 let nome = "Gorg"
-
 let xp = 0
+let nivel = ""
 
-console.log("O herói " + nome + " tem " + xp + " pontos de experiência")
+if (xp <= 1000){
+    nivel = "Ferro"
+}else if (xp > 1000 && xp <= 2000){
+    nivel = "Bronze"
+}else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+}else if (xp > 5000 && xp <= 7000){
+    nivel = "Ouro"
+}else if (xp > 7000 && xp <= 8000){
+    nivel = "Platina"
+}else if (xp > 8000 && xp <= 9000){
+    nivel = "Ascendente"
+}else if (xp > 9000 && xp <= 10000){
+    nivel = "Imortal"
+}
+else{
+    nivel = "Radiante"
+}
+
+console.log("O herói " + nome + " está no nível " + nivel)
